6f2006a44151f77f49b0d6a756ba67909502f4a3
madhuachuth <madhuachuth7@gmail.com>
2023-06-06 05:25:40 +0000

    react-router

---
+++ src/components/BlogsList/index.js
@@ -0,0 +1,47 @@
+import {Component} from 'react'
+
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+class BlogsList extends Component {
+  state = {blogsData: [], isLoader: true}
+
+  componentDidMount() {
+    this.getBlogsData()
+  }
+
+  getBlogsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    console.log(data)
+    const UpdatedBlogsData = data.map(eachItem => ({
+      id: eachItem.id,
+      author: eachItem.author,
+      avatarUrl: eachItem.avatar_url,
+      imageUrl: eachItem.image_url,
+      topic: eachItem.topic,
+      title: eachItem.title,
+    }))
+    this.setState({blogsData: UpdatedBlogsData, isLoader: false})
+  }
+
+  render() {
+    const {blogsData, isLoader} = this.state
+    return (
+      <div className="blog-list-container">
+        {isLoader ? (
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        ) : (
+          blogsData.map(item => <BlogItem blogData={item} key={item.id} />)
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogsList
